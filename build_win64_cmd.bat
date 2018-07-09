3dc00248748fb4351b5fc39a44e39037788f8bc1
Jory Schossau <joryschossau@gmail.com>
2018-07-09 15:03:23 -0400

    clarified win build scripts

--- build_win64_cmd.bat
+++ build_win64_cmd.bat
@@ -1,4 +1,5 @@
-@echo "Please run this only after running vcvars64.bat or vcvarsall.bat in your Visual Studio Build Tools"
+@echo "Please run this only after running vcvars64.bat or 'vcvarsall.bat amd64' in your Visual Studio Build Tools"
+@echo "This might be found in: C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
 @echo "building..."
 cl /w /Febuild src/src_win64_vs/*.c
 @del *.obj
--- build_win64_msys2.sh
+++ build_win64_msys2.sh
@@ -1,2 +1,3 @@
+@echo "We're assuming you have gcc installed"
 echo "building..."
 gcc -w -o build src/src_win64_gcc/*.c
